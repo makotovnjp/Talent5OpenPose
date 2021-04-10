f3ba32283dcbc2ab7f3afa8ae3d3fb8bfc82d3a3
makotovnjp <makotovnjp@gmail.com>
2021-04-10 10:09:07 +0900

    update homework

--- src/pose_estimation/open_pose_video.py
+++ src/pose_estimation/open_pose_video.py
@@ -8,9 +8,12 @@ import os
 # third party libs
 import cv2
 import torch
+import numpy as np
+import glob
 
 # Internal libs
 from utils.openpose_net import OpenPoseNet
+from utils.decode_pose import decode_pose
 
 # define constant
 OK = 0
@@ -37,7 +40,7 @@ def extract_video(file_path: str) -> int:
         ret, frame = cap.read()
         if not ret:
             break
-        cv2.imwrite('dance_' + str(frame_index) + '.jpg', frame)
+        cv2.imwrite('./data/dance_' + str(frame_index) + '.jpg', frame)
         frame_index += 1
 
     cap.release()
@@ -46,6 +49,7 @@ def extract_video(file_path: str) -> int:
     return ret_val
 
 
+# Step2: tạo các file ảnh dự đoán
 def predict(img_file_paths: typing.List, model_path: str) -> int:
     """
     step2: predict với những ảnh đã tạo ra
@@ -61,35 +65,83 @@ def predict(img_file_paths: typing.List, model_path: str) -> int:
     weights_load = {}
 
     for key in range(len(keys)):
-        print(key)
         weights_load[list(net.state_dict().keys())[key]] \
             = net_weights[list(keys)[key]]
 
-    print(weights_load)
-    #
-    # state = net.state_dict()
-    # state.update(weights_load)
-    #
-    #
-    # net.load_state_dict(state)
+    state = net.state_dict()
+    state.update(weights_load)
+    net.load_state_dict(state)
 
-    for image_path in img_file_paths:
-        # predict
-        pass
+    for index, image_path in enumerate(img_file_paths):
+        result_img = _predict_one_image(net, image_path)
+
+        cv2.imwrite('./data/predicted_images/dance_{}.jpg'.format(index), result_img)
 
 
-# Step3: nối những ảnh đã predict xong thành 1 video
-def compose_predicted_images(file_paths) -> str:
+def create_predicted_video(file_paths) -> str:
     predicted_video_path = ""
 
     return predicted_video_path
 
 
+def _predict_one_image(net: object, image_path: str) -> object:
+    """
+    predict one image
+    """
+    ori_img = cv2.imread(image_path)
+
+    # BGR->RGB
+    ori_img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
+
+    # Resize
+    size = (368, 368)
+    img = cv2.resize(ori_img, size, interpolation=cv2.INTER_CUBIC)
+    img = img.astype(np.float32) / 255.
+
+    # chuẩn hóa
+    color_mean = [0.485, 0.456, 0.406]
+    color_std = [0.229, 0.224, 0.225]
+
+    preprocessed_img = img.copy()
+
+    for i in range(3):
+        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - color_mean[
+            i]
+        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / color_std[i]
+
+    # （height 、width、colors）→（colors、height、width）
+    img = preprocessed_img.transpose((2, 0, 1)).astype(np.float32)
+
+    # cho thông tin vào tensor
+    img = torch.from_numpy(img)
+
+    x = img.unsqueeze(0)
+
+    # create heat map
+    net.eval()
+    predicted_outputs, _ = net(x)
+
+    pafs = predicted_outputs[0][0].detach().numpy().transpose(1, 2, 0)
+    heatmaps = predicted_outputs[1][0].detach().numpy().transpose(1, 2, 0)
+
+    pafs = cv2.resize(pafs, size, interpolation=cv2.INTER_CUBIC)
+    heatmaps = cv2.resize(heatmaps, size, interpolation=cv2.INTER_CUBIC)
+
+    pafs = cv2.resize(
+        pafs, (ori_img.shape[1], ori_img.shape[0]),
+        interpolation=cv2.INTER_CUBIC)
+    heatmaps = cv2.resize(
+        heatmaps, (ori_img.shape[1], ori_img.shape[0]),
+        interpolation=cv2.INTER_CUBIC)
+
+    _, result_img, _, _ = decode_pose(ori_img, heatmaps, pafs)
+
+    return result_img
+
+
 if __name__ == "__main__":
-    print("start")
     ret = extract_video('./data/dance.mp4')
-    print(ret)
     if ret == OK:
-        print("hoge")
-        ret = predict(img_file_paths=[],
+        img_file_paths = glob.glob("./data/dance_*")
+        ret = predict(img_file_paths=img_file_paths,
                       model_path='./weights/pose_model_scratch.pth')
